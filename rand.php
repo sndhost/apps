938e29bd9168a4b5c6bbe212c5d89217f137a24c
sndhost <iulianro@hotmail.com>
2015-11-12 18:27:09 +0200

    Delete rand.php

--- rand.php
+++
@@ -1,3 +0,0 @@
-
-<!-- saved from url=(0042)http://apps.clickers.info/boolean/rand.php -->
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body>0.5675177112266643</body></html>
