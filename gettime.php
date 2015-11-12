180b03e0d15147816f1d22a303f95f67b233a556
sndhost <iulianro@hotmail.com>
2015-11-12 17:39:08 +0200

    Update gettime.php

--- gettime.php
+++ gettime.php
@@ -1,1 +1,3 @@
-1446406033148
+
+<!-- saved from url=(0045)http://apps.clickers.info/boolean/gettime.php -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body>1447342603896</body></html>
