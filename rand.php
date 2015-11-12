d7f6e0fbfe0e10019a63b0bcc034d754dd955c50
sndhost <iulianro@hotmail.com>
2015-11-12 17:43:38 +0200

    Update rand.php

--- rand.php
+++ rand.php
@@ -1,1 +1,3 @@
-0.8283597265412151
+
+<!-- saved from url=(0042)http://apps.clickers.info/boolean/rand.php -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body>0.5675177112266643</body></html>
